3c056067adc636b0a533c3346689362b4120a4d7
kuflierl <41301536+kuflierl@users.noreply.github.com>
2024-05-16 20:29:55 +0200

    attempt to fix links on the main page

--- pages/_make/generate_resources.sh
+++ pages/_make/generate_resources.sh
@@ -2,6 +2,6 @@
 find resources -mindepth 1 -type d | while read folder; do
   echo "$folder" | awk -F "/" '{while (i++ < NF-1) printf "#"; printf " "; print $NF}'
   for file in `find "$folder" -maxdepth 1 -type f -printf "%f\n"`; do
-    echo "[$file]({{ site.url }}/$folder/$file)  "
+    echo "[$file]({{ \"/$folder/$file\" | relative_url }})  "
   done
 done
